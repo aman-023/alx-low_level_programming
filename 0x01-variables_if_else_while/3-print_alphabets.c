09f819d93498f123b29667b03d01d6f48aa5a134
root <amannuel0@gmail.com>
2022-07-04 08:13:15 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main- entry point
+ *
+ * Description : print lowercase letters followed by uppercase letters
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
