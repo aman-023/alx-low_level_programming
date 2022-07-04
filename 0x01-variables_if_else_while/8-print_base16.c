8fa10f90d629f36b8e0c5210ea52af3034b244a9
root <amannuel0@gmail.com>
2022-07-04 13:47:32 -0700

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,9 +10,13 @@ int main(void)
 	int x;
 
 	for (x = '0' ; x <= '9' ; x++)
+	{
 		putchar(x);
+	}
 	for (x = 'a' ; x <= 'f' ; x++)
+	{
 		putchar(x);
-		putchar('\n');
+	}
+	putchar('\n');
 	return (0);
 }
