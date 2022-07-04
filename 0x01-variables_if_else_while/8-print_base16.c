d232b651e03d110f36bdaca79fd6c7b4df70d441
root <amannuel0@gmail.com>
2022-07-04 13:36:34 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main- entry point
+ *
+ * Description - print base 16 numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0' ; x <= '9' ; x++)
+		putchar(x);
+	for (x = 'a' ; x <= 'f' ; x++)
+		putchar(x);
+	return (0);
+}
