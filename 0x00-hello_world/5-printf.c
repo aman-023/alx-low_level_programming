a1b64ca747b8efed955a6f5d7e19232811ac30ac
root <amannuel0@gmail.com>
2022-07-04 09:56:51 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
+ * main- entry point
+ *
+ * Description - print a senetence
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	getchar();
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
