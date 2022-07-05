7564e1aecae7d7b94a360b775d1975bdc517daf0
root <amannuel0@gmail.com>
2022-07-05 04:46:43 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main- entry
+ *
+ * Description - printing putchar
+ * Return: void
+ */
+int main(void)
+{
+	printPutChar();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,10 @@
+/**
+ * main- idk
+ *
+ * Description : putchar
+ * Return: explained
+ */
+int printPutChar(void)
+{
+	printf("_putchar");
+}
