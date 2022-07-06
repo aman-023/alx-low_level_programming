1bd929e64a3646cbb53f00b507a4b2dcf06eb8c9
root <amannuel0@gmail.com>
2022-07-06 10:08:04 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+/**
+ * main-check the code
+ *
+ * Description: check the code
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	for (char c = 'a' ; c <= 'z' ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
